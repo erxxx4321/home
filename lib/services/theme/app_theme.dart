7c11276606a60632d056a64565bc9e9760295680
Evonne <yufangchin@outlook.com>
2023-09-23 17:28:29 +0800

    tab panel and get products api

--- lib/services/theme/app_theme.dart
+++ lib/services/theme/app_theme.dart
@@ -4,6 +4,14 @@ final ThemeData AppTheme = ThemeData(
     scaffoldBackgroundColor: Color.fromRGBO(250, 247, 235, 1),
     appBarTheme:
         const AppBarTheme(backgroundColor: Color.fromRGBO(127, 0, 25, 1)),
+    tabBarTheme: TabBarTheme(
+        indicator: BoxDecoration(
+          color: Color.fromRGBO(127, 0, 25, 1).withOpacity(0.2),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        unselectedLabelColor: Color.fromRGBO(204, 204, 204, 1),
+        labelPadding: EdgeInsets.only(left: 20.0, right: 20.0),
+        labelColor: Color.fromRGBO(127, 0, 25, 1)),
     // drawerTheme:
     //     DrawerThemeData(backgroundColor: Color.fromRGBO(127, 0, 25, 1)),
     colorScheme: ColorScheme(
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:home/models/product.dart';
+import 'package:intl/intl.dart';
 import '../services/I10n/app_localizations.dart';
 import '../widgets/drawer.dart';
 
@@ -9,8 +14,24 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  final _tabs = [
+    Tab(text: 'chair', height: 30),
+    Tab(text: 'table', height: 30),
+    Tab(text: 'bed', height: 30),
+    Tab(text: 'lighting', height: 30),
+    Tab(text: 'sofa', height: 30),
+    Tab(text: 'cabinet', height: 30),
+  ];
+  Future<List<Product>> products = getProducts("chair");
+
   @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 6, vsync: this);
+  }
+
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
     return Scaffold(
@@ -19,7 +40,43 @@ class _HomeState extends State<Home> {
         ),
         drawer: DrawerWidget(),
         body: Column(
-          children: [Text(l10n.hello)],
+          children: [
+            TabBar(
+              isScrollable: true,
+              controller: _tabController,
+              tabs: _tabs,
+              onTap: (idx) async {
+                products = getProducts(_tabs[idx].text);
+              },
+            )
+          ],
         ));
   }
+}
+
+Future<List<Product>> getProducts(String? category) async {
+  final url = "http://localhost:3001/products";
+  final response = await http.post(
+    Uri.parse(url),
+    body: jsonEncode({"category": category}),
+    headers: {"Content-Type": "application/json"},
+  );
+  List<Product> products = [];
+  if (response.statusCode == 200) {
+    List<dynamic> jsonData = json.decode(response.body);
+    for (var j in jsonData) {
+      Product p = Product();
+      p.pid = j['_id'];
+      p.name = j['name'];
+      p.desc = j['desc'];
+      p.category = j['category'];
+      p.createdAt = DateFormat("yyyy/MM/dd").parse(j['createdAt']);
+      p.price = j['price'];
+      p.img = j['img'];
+      products.add(p);
+    }
+  } else
+    throw Exception('Failed to fetch data');
+
+  return products;
 }
